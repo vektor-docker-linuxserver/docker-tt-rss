ed0b6278c32503988c13ded4644694d72dfca5e7
vektory79 <vektory79@gmail.com>
2016-08-01 13:34:05 +0300

    Fix feeds update script

--- cron/update.sh
+++ cron/update.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /config/www/tt-rss/ && /config/www/tt-rss/update.php --feeds > /tmp/tt-rss-update.log 2>&1
-logger -f /tmp/tt-rss-update.log
+cd /config/www/tt-rss/ && /config/www/tt-rss/update.php --feeds >> /config/log/tt-rss-update.log 2>&1
